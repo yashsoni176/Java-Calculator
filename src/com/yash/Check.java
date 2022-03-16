687a3c45ee575dd44971e5b7f8b7c6a9c6a74227
= <=>
2022-03-16 15:25:12 +0530

    Multiple Exceptions

--- src/com/yash/Check.java
+++ src/com/yash/Check.java
@@ -3,12 +3,14 @@ package com.yash;
 public class Check {
     public static void main(String[] args) {
         try{
-            int x = 10, y = 0, z;
-            z = x/y;
-            System.out.println(z);
+            String name=null;
+            System.out.println(name.length());
         }
-        catch (Exception e){
-            System.out.println(e);
+        catch (ArithmeticException e){
+            System.out.println("Divide by zero not possible");
+        }
+        catch (NullPointerException e){
+            System.out.println("Null value is not accepted");
         }
     }
 }
